6b58b28b21e16f07f5a2b4a68c3d75e4c64e059f
kenyafujisaki <ken.australia0605@gmail.com>
2020-03-29 15:32:34 +0000

    PHP/Laravel14投稿データを保存しよう課題

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -95,7 +95,7 @@ return [
     'numeric' => 'The :attribute must be a number.',
     'present' => 'The :attribute field must be present.',
     'regex' => 'The :attribute format is invalid.',
-    'required' =>  ':attribute に入力が必要です。',
+    'required' => ':attribute に入力が必要です。',
     'required_if' => 'The :attribute field is required when :other is :value.',
     'required_unless' => 'The :attribute field is required unless :other is in :values.',
     'required_with' => 'The :attribute field is required when :values is present.',
@@ -146,13 +146,8 @@ return [
     */
 
     'attributes' => [
-        'name(名前)' => '名前',
-        '性別(gender)' => '性別',
-        '趣味(hobby)' => '趣味',
-        '自己紹介(introduction)' => '自己紹介',
-        
-        
-        
-        ],
+        'title' => 'タイトル',
+        'body' => '本文'
+    ],
 
 ];
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -16,21 +16,21 @@
                         </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2">氏名(name)</label></label>
+                        <label class="col-md-2">氏名(name)</label>
                         <div class="col-md-10">
                             <input type="text" class="form-control" name="title" value="{{ old('title') }}">
                         </div>
                     </div>
                     
                      <div class="form-group row">
-                        <label class="col-md-2">性別(gender)</label></label>
+                        <label class="col-md-2">性別(gender)</label>
                         <div class="col-md-10">
                             <input type="text" class="form-control" name="title" value="{{ old('title') }}">
                         </div>
                     </div>
                     
                       <div class="form-group row">
-                        <label class="col-md-2">趣味(hobby)</label></label>
+                        <label class="col-md-2">趣味(hobby)</label>
                         <div class="col-md-10">
                             <input type="text" class="form-control" name="title" value="{{ old('title') }}">
                         </div>
